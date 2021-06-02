5480cdefa14bb8946b9dd81a3dbba182f0fa381d
Nabendu <nabendu.biswas@gmail.com>
2021-06-02 11:36:45 +0530

    Final commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Image, FlatList, Dimensions, View, SafeAreaView, StyleSheet } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+const numColumns = 4;
+const tileSize = screenWidth / numColumns;
+
+const imageArr = new Array(11).fill("http://placeimg.com/640/360/tech");
+
+function renderItem({ item }) {
+  return <Image 
+    source={{ uri: item }} 
+    style={{ height: tileSize , width: tileSize }} 
+  />;
+}
 
 export default function App() {
+  const [images, setImages] = useState(imageArr);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <FlatList 
+          data={images} 
+          renderItem={renderItem} 
+          ItemSeparatorComponent={() => <View style={{ height: 3 }} />}
+          numColumns={4} 
+          key={4}
+        />
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10
   },
+  inputContainer: {
+    width: screenWidth
+  }
 });
